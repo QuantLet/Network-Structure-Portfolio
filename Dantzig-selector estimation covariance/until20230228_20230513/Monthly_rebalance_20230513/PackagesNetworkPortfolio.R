767ecd7cfd654fd593943d9e7a854d6c6cb0fb7a
wzj5163 <81182810+wzj5163@users.noreply.github.com>
2023-09-01 11:23:30 +0100

    Add files via upload

---
+++ Dantzig-selector estimation covariance/until20230228_20230513/Monthly_rebalance_20230513/PackagesNetworkPortfolio.R
@@ -0,0 +1,25 @@
+# Install and load packages
+#install.packages("IntroCompFinR", repos="http://R-Forge.R-project.org")
+libraries = c("matrixcalc", "MASS", "PortfolioAnalytics", 
+              "PerformanceAnalytics", "zoo","xts","timeSeries",
+              "plotly", "RiskPortfolios", "devtools", "PMwR","Jmisc",
+              "igraph","readxl","quadprog",
+              "viridis","hrbrthemes",
+               "FRAPO",  "R.utils", "ade4", "grDevices", "foreach", 
+               "doParallel","StepwiseTest", "stringr",  "DescTools","DT",
+              "ggpubr","knitr", "tidyr", "plotly", "rmarkdown", "gridExtra", "reticulate",
+              "fPortfolio", 
+              "xtable", "DEoptim", "ROI", "ROI.plugin.quadprog", "ROI.plugin.glpk",
+              "nlshrink", "psych", 
+              "quantmod", 
+              "tcltk2","limSolve",
+              "MTS","Matrix","pracma","glasso","MST", "IntroCompFinR",
+              "RColorBrewer", "broom", "egg", "nloptr", "fAssets",
+              "magick") 
+              
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+
